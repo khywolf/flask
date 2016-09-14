37a7332b51eaa17e6bb65117cd3940b629828ead
khywolf <khywolf@126.com>
2016-09-14 17:06:15 +0800

    555

---
+++ webapp/controllers/main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+from flask import render_template, Blueprint
+from webapp.forms import LoginForm, RegisterForm
+from webapp.models import db, Post, Tag, Comment, User, tags
+
+main_blueprint = Blueprint(
+    'main',
+    __name__,
+    template_folder='../template/main'
+)
+
+@main_blueprint.route('/')
+def index():
+    return redirect(url_for('blog.main'))
+
+@main_blueprint.route('/login', methods['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit();
+        flash("You have been logged in.", category="success")
+        return redirect(url_for('blog.home'))
+
+    return render_template('login.html', form=form)
+
+@main_blueprint.route('/logout', methods=['GET', 'POST'])
+def logout():
+    flash("You have been logged out.", category="success")
+    return redirect(url_for('.home'))
+
+@main_blueprint.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        new_user = User()
+        new_user.username = form.username.data
+        new_user.set_password(form.password.data)
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash("Your user has been created, please login.", category="success")
+        return redirect(url_for('.login'))
+
+    return render_template('reister.html', form=form)
